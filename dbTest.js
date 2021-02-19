483f54b962be1a85f5ee5157acc7aaa014ef9b22
darrengp <ceo.darrenperry@gmail.com>
2021-02-19 08:10:46 -0800

    basic crud done

--- dbTest.js
+++ dbTest.js
@@ -23,26 +23,129 @@ models.hunter.create({
 });
 */
 ///////////////////////////////////////////
-/*
+
 // 3.
-async function creating() {
+// async function creating() {
+//   try {
+//     // simple create
+//     const hunter = await db.hunter.create({
+//       name: "Dengar",
+//       client: "Mercurial Swift",
+//       active: false,
+//     });
+//     console.log(hunter);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// creating();
+
+///////////////////////////
+// 4.
+/*
+async function reading() {
   try {
-    // simple create
-    const hunter = await db.hunter.create({
-      name: "Bobba Fett",
-      client: "Jabba the Hut",
-      active: true,
-    });
-    console.log(hunter);
+    const bounties = await db.bounty.findAll();
+    console.log(bounties);
   } catch (error) {
     console.log(error);
   }
 }
-creating();
+reading();
 */
-///////////////////////////
-// 4.
 
+//////////////////////////////////////////////
+// 5.
+// async function reading() {
+//   try {
+//     const activeHunters = await db.hunter.findOne(
+//       {
+//         where: {
+//           active = t,
+//         }
+//       }
+//     );
+//     console.log(activeHunters);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// reading();
+
+//////////////////////////////////////////////
+
+// 6.
+
+// async function updating() {
+//   try {
+//     const numRowsUpdated = await db.bounty.update(
+//       {
+//         captured: true,
+//       },
+//       {
+//         where: {
+//           captured: false,
+//         },
+//       }
+//     );
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// updating();
+
+/////////////////////////////////////////////////////
+
+// 7.
+// async function destroying() {
+//   try {
+//     const numOwnersDestroyed = await db.hunter.destroy({
+//       where: {
+//         name: "Dengar",
+//       },
+//     });
+//     console.log(numOwnersDestroyed);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// destroying();
+// personal notes below will remove
+////////////////////////////////////////
+// async function reading() {
+//   try {
+//     // console.log(owners);
+//     // owners.forEach((owner) => {
+//     //   console.log(`${owner.name} is ${owner.age}  years old`);
+//     // });
+//     ///////////////
+//     // const owners = await db.owner.findAll();
+//     // const william = await db.owner.findOne({
+//     //   where: {
+//     //     name: "William",
+//     //   },
+//     // });
+//     // console.log(`found ${william.name}`);
+//     //////////////////////
+//     const owners = await db.owner.findAll({
+//       include: db.property,
+//     });
+
+//     owners.forEach((owner) => {
+//       owner.properties.forEach((property) => {
+//         console.log(property);
+//       });
+//     });
+//     // console.log(owners);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// reading();
+
+////////////////////////////////////////////////////
 /*
 async function creating() {
   try {
