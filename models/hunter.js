161f43dc1fa1e8c4cba946cffa2ff5f4df4062ae
wonkatin <j.drawers@gmail.com>
2021-02-21 12:34:41 -0600

    Created Relationships

--- models/hunter.js
+++ models/hunter.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.hunter.belongsTo(models.bounty)
     }
   };
   hunter.init({
